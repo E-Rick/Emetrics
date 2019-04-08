6d6d8275fdfe82e6e7411348bbfebb5471e7577a
E-rick <erickmartinezjunior@gmail.com>
2019-04-08 17:21:04 -0400

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -18,7 +18,8 @@ passport.use(
 		{
 			clientID     : process.env.GOOGLE_ID,
 			clientSecret : process.env.GOOGLE_KEY,
-			callbackURL  : 'http://localhost:5000/auth/google/callback'
+			callbackURL  : '/auth/google/callback',
+			proxy        : true
 		},
 		async (accessToken, refreshToken, profile, done) => {
 			// Check if we already have a user with that Google profile ID
