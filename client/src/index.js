43ac6b0924edc205d202ceca17284d04704ea57e
E-rick <erickmartinezjunior@gmail.com>
2019-04-10 17:47:20 -0400

    Add redux-thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -12,9 +13,11 @@ import reducers from './reducers';
   @param function reducers - pass the all the reducers combined from ./reducers directory's index.js
   @param object {} - sets up the initial state of the app (we leave null because no setup needed)
                      this param is more relevant for server-side rendering
-  @param function applyMiddleWare() - invokes redux helper to hookup middlewares  
+  @param function applyMiddleWare() - invokes redux helper to hookup middle-wares
+  * @param reduxThunk - middleware to allow for async logic to interacts with the store,
+                        like simply async AJAX requests and complex synchronous logic
 */
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 /* 
   @desc Sets up Provider component that reads changes from the redux store and any time the 
