1ee39feb094246e668a49469e595dcafe7126d5f
E-rick <erickmartinezjunior@gmail.com>
2019-04-10 17:44:00 -0400

    Add comments

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,8 @@
 const proxy = require('http-proxy-middleware');
-
+/* 
+	Setups the proxy to connect the front end to the backend node server in development environment 
+	Note: No proxy needed for production environment 
+*/
 module.exports = function(app) {
 	app.use(proxy([ '/api', '/auth/google' ], { target: 'http://localhost:5000' }));
 };
