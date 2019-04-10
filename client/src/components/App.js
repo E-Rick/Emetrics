dd0a0b345a39b445c3bb9c35f6dcb88145921e8b
E-rick <erickmartinezjunior@gmail.com>
2019-04-10 14:59:59 -0400

    Setup react-router-dom routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 /* 
-	@param BrowserRouter - react-router-dom helper that is the brains of react-router and tells it how
+	@param BrowserRouter - react component that is the brains of react-router and tells it how
 		to behave. It looks at the current url and changes a set of components that are visible at any time
 	@param Route - react component that is used to setup a rule between a certain route that the user
 		might visit  and a set of components that will be actually visible
 */
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-	return <div>Hi There!</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div className='container'>
+					<Header />
+					<Route exact path='/' component={Landing} />
+					<Route exact path='/surveys' component={Dashboard} />
+					<Route path='/surveys/new' component={SurveyNew} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
