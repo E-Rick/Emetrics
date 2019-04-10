3fcf80423d41f01afb1b70dbf46be58d1d8ff495
E-rick <erickmartinezjunior@gmail.com>
2019-04-10 12:52:21 -0400

    Add react-router-dom
    Import react-router-dom helpers and add dummy components to test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+/* 
+	@param BrowserRouter - react-router-dom helper that is the brains of react-router and tells it how
+		to behave. It looks at the current url and changes a set of components that are visible at any time
+	@param Route - react component that is used to setup a rule between a certain route that the user
+		might visit  and a set of components that will be actually visible
+*/
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
 	return <div>Hi There!</div>;
