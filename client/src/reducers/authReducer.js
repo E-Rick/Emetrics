29997e3ceabafe61212a9a456c48fdac63f0c3af
E-rick <erickmartinezjunior@gmail.com>
2019-04-11 19:41:23 -0400

    Edit authReducer return values + add comments

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,19 @@
+import { FETCH_USER } from '../actions/types';
 /* 
-  @desc authReducer.js is a reducer that records whether or not the user is logged in
-  @param object state - sets up the initial state (none needed right now)
+	@desc authReducer.js is a reducer that records whether or not the user is logged in
+		3 different states are returned:
+			1. null - the default case to indicate we are waiting for our fetch_user action ajax request
+				to return. (waiting for request)
+			2. action.payload - the payload contains the google profile. (user is logged in)
+			3. false - the fetch_user action.payload returned '' a falsey value. (user not logged in) 
+	@param object state - sets up the initial state to null indicating we are waiting for 
+		the fetch_user ajax request to return something
   @param object action - 
 */
-export default function(state = {}, action) {
+export default function(state = null, action) {
 	switch (action.type) {
-		// default action no change to state just return state object
+		case FETCH_USER:
+			return action.payload || false;
 		default:
 			return state;
 	}
