f352c2e02b383a7576f09c57a61803ade8e168ac
E-rick <erickmartinezjunior@gmail.com>
2019-04-19 20:38:57 -0400

    Update User schema to have credits

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	googleId : String
+	googleId : String,
+	credits  : { type: Number, default: 0 } // credits to purchase surveys
 });
 
 module.exports = mongoose.model('User', userSchema);
