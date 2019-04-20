21f8397f25035068ab98fd7bb494ea9094def831
E-rick <erickmartinezjunior@gmail.com>
2019-04-19 20:37:57 -0400

    Add react-stripe-checkout component

---
+++ client/src/components/StripeWrapper.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class StripeWrapper extends Component {
+	render() {
+		return (
+			<StripeCheckout
+				name='Emetrics' // the pop-in header title
+				description='$5 for 5 email credits' // the pop-in header sub-title
+				amount={500} // cents
+				currency='USD'
+				token={token => this.props.handleToken(token)} // submit callback action creator
+				stripeKey={process.env.REACT_APP_STRIPE_KEY}>
+				<button className='btn'>Add Credits</button>
+			</StripeCheckout>
+		);
+	}
+}
+
+export default connect(null, actions)(StripeWrapper);
