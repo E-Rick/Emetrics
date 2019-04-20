510a3be0c06aa0aa4749011374f6f4fd911fe7b9
E-rick <erickmartinezjunior@gmail.com>
2019-04-19 20:31:50 -0400

    Add handleToken action creator
    Action creator used for Stripe API integration

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,3 +9,13 @@ export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/current_user'); // get response from back end server
 	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
 };
+
+/* 
+  @desc declares the handleToken action creator that sends the Stripe token returned from User 
+  submitting a payment to our back-end API updating the User model with the updated credits amount
+  @param token - Stripe token from StripeCheckout component
+*/
+export const handleToken = token => async dispatch => {
+	const res = await axios.post('/api/stripe', token); // response containing updated user model
+	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
+};
