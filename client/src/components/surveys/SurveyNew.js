f212c68e5c2467f581448166d2fc32773c26aaa0
E-rick <erickmartinezjunior@gmail.com>
2019-04-24 02:03:45 -0400

    Add SurveyNew component
    Shows SurveyForm and SurveyFormReview

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
+
+/**
+ * Shows SurveyForm and SurveyFormReview
+ */
+export default class SurveyNew extends Component {
+	render() {
+		return (
+			<div>
+				<SurveyForm />
+			</div>
+		);
+	}
+}
