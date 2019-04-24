460ec1671a8c2253d738a4a1ebc9400e9ac18218
E-rick <erickmartinezjunior@gmail.com>
2019-04-24 02:03:01 -0400

    Add redux form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 /* 
   @desc exports combineReducers() invocation from redux that 
-  @param object {} - contains the keys that represent the keys inside of our state object
+  @param object {} - contains the keys that represent the reducers inside of our state object
 */
 export default combineReducers({
-	auth : authReducer //auth state is being produced or manufactured by the authReducer
+	auth : authReducer, // auth state is being produced or manufactured by the authReducer
+	form : formReducer // redux-form reducer
 });
