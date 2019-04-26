c8ff37cd94e7cad0e08b81af9e7c73a1cfce5c1f
Unkwn De Andrade <unkwn@Unkwns-MacBook-Pro.local>
2019-04-26 18:32:53 -0400

    Add SurveyField validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 /** 
  * SurveyField contains logic to render a single label and text input
- * @arg {*} input es6 de-structuring of props.input  
+ * @param {*} input props.input - input object from redux-form
+ * @param {*} label props.label - label for each SurveyField input
+ * @param {*} error props.meta.error - a string with error name else null string
+ * @param {*} touched props.meta.touched - a boolean that returns true if user clicks
+ * 	in the field and then clicks out
  */
-const SurveyField = ({ input, label }) => {
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
 	return (
 		<div>
 			<label>{label}</label>
 			<input {...input} />
+			{/* if user touches input and validation returns an error, render error */}
+			{touched && error}
 		</div>
 	);
 };
