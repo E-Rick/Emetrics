5a697761126478f7acca46525759c4ee35baf37a
E-Rick <emarti33@binghamton.edu>
2019-04-27 18:09:45 -0400

    Add form validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,20 +1,23 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form'; //
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
-	{ label: 'Survey Title', name: 'title' },
-	{ label: 'Subject Line', name: 'subject' },
-	{ label: 'Email Body', name: 'body' },
-	{ label: 'Recipient List', name: 'emails' }
+	{ label: 'Survey Title', name: 'title', noValueError: 'You must provide a survey title' },
+	{ label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject' },
+	{ label: 'Email Body', name: 'body', noValueError: 'You must provide a body' },
+	{ label: 'Recipient List', name: 'emails', noValueError: 'You must provide a recipient list' }
 ];
 
 /**
  * Shows a form for a user to add input
  */
 class SurveyForm extends Component {
-	//
+	// helper function to iterate over FIELDS array and for every object, create
+	// and return one custom redux field object that uses the props label and name
 	renderFields() {
 		return _.map(FIELDS, ({ label, name }) => {
 			return <Field key={name} component={SurveyField} text='text' label={label} name={name} />;
@@ -26,11 +29,39 @@ class SurveyForm extends Component {
 			<div>
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
 					{this.renderFields()}
-					<button type='submit'>Submit</button>
+					<Link to='/surveys' className='red btn-flat white-text'>
+						Cancel
+					</Link>
+					<button type='submit' className='teal btn-flat right white-text'>
+						Next
+						<i className='material-icons right'>done</i>
+					</button>
 				</form>
 			</div>
 		);
 	}
 }
 
-export default reduxForm({ form: 'surveyForm' })(SurveyForm);
+/**
+ * Redux-form helper function that adds validation to the form
+ * @param {*} values Object containing all the values coming from the SurveyForm
+ * @return {errors}
+ */
+function validate(values) {
+	const errors = {};
+
+	errors.emails = validateEmails(values.emails || '');
+
+	// add errors with matching values props
+	_.each(FIELDS, ({ name, noValueError }) => {
+		if (!values[name]) errors[name] = noValueError;
+	});
+
+	return errors;
+}
+
+// pass in validate function to be run every submit
+export default reduxForm({
+	validate,
+	form     : 'surveyForm'
+})(SurveyForm);
