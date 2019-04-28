8f8a10d823dfd8a83fa57d01caaa1f6952e76cd6
E-Rick <emarti33@binghamton.edu>
2019-04-28 13:38:14 -0400

    Add formFields.js
    Add array of survey form fields

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,7 @@
+// Array of the survey form fields
+export default [
+	{ label: 'Survey Title', name: 'title', noValueError: 'You must provide a survey title' },
+	{ label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject' },
+	{ label: 'Email Body', name: 'body', noValueError: 'You must provide a body' },
+	{ label: 'Recipient List', name: 'recipients', noValueError: 'You must provide a recipient list' }
+];
