a7fff02dcc3b6ccdb9f3f5b1ec067713ae5bf5ac
E-Rick <emarti33@binghamton.edu>
2019-04-28 13:38:38 -0400

    Add submitSurvey action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-/* 
-  @desc declares the fetchUser action creator of type FETCH_USER and dispatches the action object
-  containing the action type and payload from the axios response to the reducer authReducer.js
-*/
+/**
+ * @desc declares the fetchUser action creator of type FETCH_USER and dispatches the action object
+ * containing the action type and payload from the axios response to the reducer authReducer.js
+ */
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/user'); // get response from back end server
 	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
 };
 
-/* 
-  @desc declares the handleToken action creator that sends the Stripe token returned from User 
-  submitting a payment to our back-end API updating the User model with the updated credits amount
-  @param token - Stripe token from StripeCheckout component
-*/
+/**
+ * @desc declares the handleToken action creator that sends the Stripe token returned from User
+ * submitting a payment to our back-end API updating the User model with the updated credits amount
+ * @param token - Stripe token from StripeCheckout component
+ */
 export const handleToken = token => async dispatch => {
 	const res = await axios.post('/api/stripe', token); // response containing updated user model
+	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
+};
+
+/**
+ * @desc declares submitSurvey action creator that posts the survey to back-end API
+ * and then dispatches a FETCH_USER action to update the header with new User credits data
+ * @param {*} values survey form values from SurveyForm
+ */
+export const submitSurvey = (values, history) => async dispatch => {
+	const res = await axios.post('/api/surveys', values); // response containing updated user model
+	history.push('/surveys'); // redirect to /surveys after survey submit
 	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
 };
