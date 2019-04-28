e55e1da6c2a736ede298acb35389f85908487d11
E-Rick <emarti33@binghamton.edu>
2019-04-28 13:41:04 -0400

    Add logic to switch survey views
    Add reduxForm helper to unmount survey values upon exit
    Add component level state to switch survey form view

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 /**
  * Shows SurveyForm and SurveyFormReview
  */
-export default class SurveyNew extends Component {
-	render() {
+class SurveyNew extends Component {
+	// initialize component level state
+	state = { showFormReview: false };
+
+	// helper function to render content
+	renderContent() {
+		if (this.state.showFormReview)
+			return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />;
 		return (
-			<div>
-				<SurveyForm />
-			</div>
+			<SurveyForm
+				onSurveySubmit={() => {
+					this.setState({ showFormReview: true });
+				}}
+			/>
 		);
 	}
+
+	render() {
+		return <div>{this.renderContent()}</div>;
+	}
 }
+
+// connect SurveyNew to the reduxForm so form values unmount when navigating away from SurveyNew
+export default reduxForm({
+	form : 'surveyForm'
+})(SurveyNew);
