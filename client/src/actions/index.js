a14470545f68321d5871aea96421e82e882a796d
E-rick <erickmartinezjunior@gmail.com>
2019-05-10 00:23:42 -0400

    Add fetchSurveys action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 /**
  * @desc declares the fetchUser action creator of type FETCH_USER and dispatches the action object
@@ -24,9 +24,20 @@ export const handleToken = token => async dispatch => {
  * @desc declares submitSurvey action creator that posts the survey to back-end API
  * and then dispatches a FETCH_USER action to update the header with new User credits data
  * @param {*} values survey form values from SurveyForm
+ * @param {*} history 
  */
 export const submitSurvey = (values, history) => async dispatch => {
 	const res = await axios.post('/api/surveys', values); // response containing updated user model
 	history.push('/surveys'); // redirect to /surveys after survey submit
 	dispatch({ type: FETCH_USER, payload: res.data }); //dispatch response data
+};
+
+/**
+ * @desc declares the fetchSurveys action creator that gets an array of surveys for the current 
+ * user from the back-end API and then dispatches a FETCH_SURVEYS action with the survey 
+ * array in res.data
+ */
+export const fetchSurveys = () => async dispatch => {
+	const res = await axios.get('/api/surveys');
+	dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
