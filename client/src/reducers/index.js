cd32d3c08ee4ed48e46f4e5e75b1a8fdff8b28e3
E-rick <erickmartinezjunior@gmail.com>
2019-05-10 00:23:01 -0400

    Add fetch surveys action creator/reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
-/* 
-  @desc exports combineReducers() invocation from redux that 
-  @param object {} - contains the keys that represent the reducers inside of our state object
-*/
+/**
+ * @desc exports combineReducers() invocation from redux that 
+ * @param object {} - contains the keys that represent the reducers inside of our state object 
+ */
 export default combineReducers({
-	auth : authReducer, // auth state is being produced or manufactured by the authReducer
-	form : formReducer // redux-form reducer
+	auth    : authReducer, // auth state is being produced or manufactured by the authReducer
+	form    : formReducer, // redux-form reducer
+	surveys : surveysReducer // survey state is being produced or manufactured by the surveysReducer
 });
