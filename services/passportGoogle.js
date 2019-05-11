1efff9ffbaf513ab3e6c044093e6d0028a5c9d9c
E-rick <erickmartinezjunior@gmail.com>
2019-05-11 01:47:39 -0400

    Update Google auth strategy

--- services/passportGoogle.js
+++ services/passportGoogle.js
@@ -22,12 +22,18 @@ passport.use(
 			proxy        : true
 		},
 		async (accessToken, refreshToken, profile, done) => {
+			const email = profile.emails[0].value;
 			// Check if we already have a user with that Google profile ID
-			const existingUser = await User.findOne({ googleId: profile.id });
+			const existingUser = await User.findOne({ email: email });
 			//  we already have a record with the given profile ID
 			if (existingUser) return done(null, existingUser);
 			// we don't have a record with this ID, make a new record
-			const user = await new User({ googleId: profile.id }).save();
+			const user = await new User({
+				oauthId : profile.id,
+				email   : email,
+				name    : profile.displayName,
+				avatar  : profile.photos[0].value
+			}).save();
 			done(null, user);
 		}
 	)
